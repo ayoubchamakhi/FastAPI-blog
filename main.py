357c0537a68d45ca5217a74f3f4a8b085a258018
Ayoub CHAMAKHI <ayoubchammakhi@gmail.com>
2026-02-08 00:32:57 +0100

    add pydantic schema

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -36,7 +37,24 @@ def home(request: Request):
         {"posts": posts, "title" : "Home"},
         )
 
-@app.get("/posts/{post_id}", include_in_schema=False)
+@app.post(
+        "/api/posts",
+        response_model=PostResponse,
+        status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025"
+    }
+    posts.append(new_post)
+    return new_post
+
+@app.get("/posts/{post_id}", include_in_schema=False, response_model=PostResponse)
 def get_post(request: Request, post_id: int):
     for post in posts:
         if post.get("id") == post_id:
@@ -48,7 +66,7 @@ def get_post(request: Request, post_id: int):
         )
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
